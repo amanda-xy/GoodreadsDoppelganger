a101b1c03a9cb3ccaa6d997d0b4ec2e892d08914
amanda-xy <erikssonmandaa@gmail.com>
2021-06-12 21:54:32 +0200

    :)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -14,3 +14,18 @@
 //        document.getElementById("header").style.fontSize = "90px";
 //    }
 //}
+
+function ratingRangeChanged(val) {
+
+    var element = document.getElementById('rangevalue');
+    if (val == 1)
+        element.value = "1 - Really bad"
+    else if (val == 2)
+        element.value = "2 - Bad"
+    else if (val == 3)
+        element.value = "3 - Okay"
+    else if (val == 4)
+        element.value = "4 - Good"
+    else if (val == 5)
+        element.value = "5 - Really good!"
+}
